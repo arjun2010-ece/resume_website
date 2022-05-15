2fdfbd32c3d5eefafb5fa67ba46339a9f2293618
ARJUN SAH <arjunsah@ARJUNs-MacBook-Pro.local>
2022-05-15 09:05:05 +0100

    Updates

---
+++ precache-manifest.35ca4f329a6748699c99d082baf452c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b10dadee480338c5fc629cb707682c",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "cdd2db5570e7fadf496f",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "3a43d44cb38bb1f7c693",
+    "url": "/home/static/js/2.93d8d3cb.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.93d8d3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd2db5570e7fadf496f",
+    "url": "/home/static/js/main.b2b65a90.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  }
+]);
